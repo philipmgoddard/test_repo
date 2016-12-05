9fecec553feee23014a5e4a5b3665d908934bb6c
Philip Goddard <pmgoddard89@gmail.com>
2016-12-05 20:48:54 +0000

    changes

--- hello.py
+++ hello.py
@@ -1,14 +1,15 @@
-print("hello world")
+print("hello snoodle")
 
 class Dexter():
     def __init__(self, hats):
         self.rats = hats * 3
 
     def __call__(self):
-        print([y for x in range(10) if x != 1])
+        print([x for x in range(4) if x != 2])
 
-class Phil():
+class Phil(Exception):
     def __init__(self):
+        self.frogs = 10
 
 print("SOME ANNOYING INSERTIONS)
 
